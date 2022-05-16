61cd9759c127ae1f94d7a513c81ff2aad65b7148
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-16 15:00:39 +0530

    uc2 RollingDie

---
+++ SnakeAndLadder/SnakeAndLadder/RollingDie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnackLadderGame
+{
+    internal class RollingDie
+    {
+        public void NumGenerator()
+        {
+            Random LudoNum = new Random();
+            int Num = LudoNum.Next(1, 6);
+            Console.WriteLine("You got the number: " +Num);
+        }
+    }
+}
